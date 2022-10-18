f44e5e738f8fc58f689c810bc166a335f2e76fe4
Ptunc <spolentunc@gmail.com>
2022-10-18 17:06:27 +0300

    Just Commit

---
+++ java9.java
@@ -0,0 +1,30 @@
+package Java9;
+
+import java.util.Scanner;
+
+public class java9 {
+    public static void main(String[] args) {
+        
+        int sifre, kars, a;
+        String kontrol;
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("sifreyi girin: ");
+        sifre = input.nextInt();
+        kars = sifre;
+        System.out.print("sifre sıfırlamak istiyor musunuz? (1/0): ");
+        a = input.nextInt();
+        
+        if(a==1){
+            System.out.print("yeni sifreyi girin: ");
+            sifre = input.nextInt();
+            kontrol = kars == sifre ? "Şifre oluşturulamadı. lütfen tekrar girin." : "sifre oluşturuldu.";
+            System.out.println(kontrol);
+        }
+            else{
+            System.out.println("Tamamdır.");
+            }
+
+    }
+}
